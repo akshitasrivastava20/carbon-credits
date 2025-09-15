a0081af2dfa9533928403667787a2ce89d2f3994
Your Name <akshita.srivastava.1505@gmail.com>
2025-09-15 11:22:44 +0530

    ddn

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-screen p-6">
-      <SignUp />
+      <SignUp fallbackRedirectUrl="/register" />
     </div>
   )
 }
